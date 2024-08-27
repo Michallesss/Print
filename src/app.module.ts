859570e5fa3c0ffb7a19ba8ed2564611209dac45
Mikeal <mwiemszra@gmail.com>
2024-08-27 19:18:21 +0200

    💡 Add todos

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,3 +1,6 @@
+// TODO: 
+// 1. ? Add devtools (https://docs.nestjs.com/devtools/overview)
+// 2. ? Add rate limiting (https://docs.nestjs.com/security/rate-limiting)
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
