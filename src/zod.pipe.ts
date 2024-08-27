6caa0e8a34c2f6f76c9d1b5c3b4715ca309e434d
Mikeal <mwiemszra@gmail.com>
2024-08-27 19:56:46 +0200

    📦 Create zod validation pipe

---
+++ src/zod.pipe.ts
@@ -0,0 +1,16 @@
+// https://docs.nestjs.com/pipes
+import { PipeTransform, ArgumentMetadata, BadRequestException } from '@nestjs/common';
+import { ZodSchema  } from 'zod';
+
+export class ZodValidationPipe implements PipeTransform {
+  constructor(private schema: ZodSchema) {}
+
+  transform(value: unknown, metadata: ArgumentMetadata) {
+    try {
+      const parsedValue = this.schema.parse(value);
+      return parsedValue;
+    } catch (error) { // todo check error object for error message to return
+      throw new BadRequestException('Validation failed');
+    }
+  }
+}
