7b93164df841e3501210f5c2a980c52696d17d0f
Mikeal <mwiemszra@gmail.com>
2024-08-27 19:49:06 +0200

    📦 Initialize prisma

---
+++ src/prisma.service.ts
@@ -0,0 +1,10 @@
+// https://docs.nestjs.com/recipes/prisma
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit {
+  async onModuleInit() {
+    await this.$connect();
+  }
+}
