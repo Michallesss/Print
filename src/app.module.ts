7abc0f78d850ca149d841dced52646ec83ec363f
Mikeal <mwiemszra@gmail.com>
2024-08-28 18:09:45 +0200

    📦 Initialize env

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,19 @@
 // 1. ? Add devtools (https://docs.nestjs.com/devtools/overview)
 // 2. ? Add rate limiting (https://docs.nestjs.com/security/rate-limiting)
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 // import { PrismaService } from './prisma.service';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    UsersModule, 
+    AuthModule
+  ],
   controllers: [AppController],
   providers: [AppService, /*PrismaService*/], // Prisma service must be initialized with db at first
 })
